d4574b95ea35ea31b6ca36810b4ea7689ca978b0
daniloxxv <danilosoares@gmail.com>
2019-11-03 09:38:33 +0000

    Playing with styled components

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -5,7 +5,7 @@ import {Title } from './styles'
 export default function Main(){
     let seconds = new Date().getSeconds()
     return <Title seconds={seconds}>Hello, Main
-        <small>Just kidding</small>
+        <small>Just testing something</small>
 
     </Title>
 }
