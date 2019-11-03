b313573887f13719aa9e3eb39eb4eb5c3e152612
daniloxxv <danilosoares@gmail.com>
2019-11-03 11:22:21 +0000

    Retrieving api details

--- src/pages/Repository/index.js
+++ src/pages/Repository/index.js
@@ -1,6 +1,40 @@
-import React from 'react'
+import React, {Component} from 'react'
+import api from '../../services/api'
 
-export default function Repository({match}){
+export default class Repository extends Component {
+    
+    state = {
+        repository: {},
+        issues: [],
+        loading: true
+    }
 
-    return <h1>Repository: {decodeURIComponent(match.params.repository)}</h1>
-}
+    async componentDidMount(){
+        const {match} = this.props
+        const repoName = decodeURIComponent(match.params.repository)
+
+        const [repository, issues] = await Promise.all([
+            api.get(`/repos/${repoName}`),
+            api.get(`/repos/${repoName}/issues`, {
+                params: {
+                    state: 'open',
+                    per_page: 5
+                }
+            })
+        ])
+
+        this.setState({
+            repository: repository.data,
+            issues: issues.data,
+            loading: false
+        })
+
+    }
+
+    render(){
+        return (
+            <h1>Repository: {this.repoName}</h1>
+        )
+    }
+
+}   
