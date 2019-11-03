d37185c50a37e00e6ee1c808dc0188dc3b39e825
daniloxxv <danilosoares@gmail.com>
2019-11-03 09:50:10 +0000

    Setting up global styles

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Routes from './routes'
+import GlobalStyle from './styles/global'
 
 function App() {
   return (
-    <Routes>
-      
-    </Routes>
+    <>
+      <Routes />
+      <GlobalStyle />
+    </>
   );
 }
 
