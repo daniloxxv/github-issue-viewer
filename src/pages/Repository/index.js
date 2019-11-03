628693a6fd9a83cbec908b543fbdfb6ad33bdc90
daniloxxv <danilosoares@gmail.com>
2019-11-03 09:25:26 +0000

    Setting up routes

---
+++ src/pages/Repository/index.js
@@ -0,0 +1,5 @@
+import React from 'react'
+
+export default function Repository(){
+    return <h1>Hello, Repository</h1>
+}
---
+++ src/routes.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import {BrowserRouter, Switch, Route} from 'react-router-dom'
+import Main from './pages/Main'
+import Repository from './pages/Repository'
+
+
+export default function Routes () {
+    return (
+    <BrowserRouter>
+    <Switch>
+            <Route exact path="/" component={Main}></Route>
+            <Route path="/repository" component={Repository}></Route>
+
+    </Switch>
+    </BrowserRouter>
+    )
+}
